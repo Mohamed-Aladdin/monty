4085ac8e58cf4f5f1bf345c91a6a8d7935f165ad
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-16 19:11:20 +0000

    Finish numeric op helper fns

--- op_helpers2.c
+++ op_helpers2.c
@@ -10,10 +10,10 @@ void m_sub(stack_t **h, __attribute__((unused))unsigned int ipn)
 {
 	stack_t *temp;
 
-	if (*stack && (*stack)->next)
+	if (*h && (*h)->next)
 	{
 		(*h)->next->n -= (*h)->n;
-		freeable = *h;
+		temp = *h;
 		*h = (*h)->next;
 		(*h)->prev = NULL;
 		free(temp);
@@ -54,6 +54,71 @@ void m_div(stack_t **h, __attribute__((unused))unsigned int ipn)
 	if (*h && (*h)->next)
 	{
 		(*h)->next->n /= (*h)->n;
+		temp = *h;
+		*h = (*h)->next;
+		(*h)->prev = NULL;
+		free(temp);
+	}
+}
+
+/**
+ * m_mul - multiplies second top element of stack with top element of stack.
+ * @h: The given stack.
+ * @ipn: The input line number.
+ *
+ * Return: Void.
+ */
+
+void m_mul(stack_t **h, __attribute__((unused))unsigned int ipn)
+{
+	stack_t *temp;
+
+	if (*h && (*h)->next)
+	{
+		(*h)->next->n *= (*h)->n;
+		temp = *h;
+		*h = (*h)->next;
+		(*h)->prev = NULL;
+		free(temp);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", driver.ipn);
+		clean_up();
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * m_mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack.
+ * @h: The given stack.
+ * @ipn: The input line number.
+ *
+ * Return: Void.
+ */
+
+void m_mod(stack_t **h, __attribute__((unused))unsigned int ipn)
+{
+	stack_t *temp;
+
+	if (!*h || !(*h)->next)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", driver.ipn);
+		clean_up();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*h)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", driver.ipn);
+		clean_up();
+		exit(EXIT_FAILURE);
+	}
+
+	if (*h && (*h)->next)
+	{
+		(*h)->next->n %= (*h)->n;
 		temp = *h;
 		*h = (*h)->next;
 		(*h)->prev = NULL;
