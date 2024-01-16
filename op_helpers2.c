e65f4fce99f129155b39bc85a7c87827b0c4a574
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-16 19:23:28 +0000

    Add rotl and rotr fns

--- op_helpers2.c
+++ op_helpers2.c
@@ -125,3 +125,65 @@ void m_mod(stack_t **h, __attribute__((unused))unsigned int ipn)
 		free(temp);
 	}
 }
+
+/**
+ * m_rotl - rotates the stack to the top.
+ * @h: The given stack.
+ * @ipn: The input line number.
+ *
+ * Return: Void.
+ */
+
+void m_rotl(stack_t **h, __attribute__((unused))unsigned int ipn)
+{
+	stack_t *ft, *sd;
+
+	if (!*h || !(*h)->next)
+	{
+		return;
+	}
+
+	ft = *h;
+	sd = (*h)->next;
+	*h = sd;
+	sd->prev = NULL;
+
+	while (sd->next)
+	{
+		sd = sd->next;
+	}
+	sd->next = ft;
+	ft->next = NULL;
+	ft->prev = sd;
+}
+
+/**
+ * m_rotr - rotates the stack to the bottom.
+ * @h: The given stack.
+ * @ipn: The input line number.
+ *
+ * Return: Void.
+ */
+
+void m_rotr(stack_t **h, __attribute__((unused))unsigned int ipn)
+{
+	stack_t *ft, *sd;
+
+	if (!(*h) || !(*h)->next)
+	{
+		return;
+	}
+
+	ft = *h;
+
+	while (ft->next->next)
+	{
+		ft = ft->next;
+	}
+	sd = ft->next;
+	ft->next = NULL;
+	(*h)->prev = sd;
+	sd->next = *h;
+	sd->prev = NULL;
+	*h = sd;
+}
