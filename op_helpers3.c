07a826516e2e3c9da495e4740435b6546af322d5
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-16 19:34:50 +0000

    Add stack & queues identifier fns

--- op_helpers3.c
+++ op_helpers3.c
@@ -28,3 +28,33 @@ void m_rotr(stack_t **h, __attribute__((unused))unsigned int ipn)
 	sd->prev = NULL;
 	*h = sd;
 }
+
+/**
+ * m_stack - sets the format of the data to a stack (LIFO).
+ * @h: The given stack.
+ * @ipn: The input line number.
+ *
+ * Return: Void.
+ */
+
+void m_stack(stack_t **h, unsigned int ipn)
+{
+	(void)h;
+	(void)ipn;
+	driver.is_queue = false;
+}
+
+/**
+ * m_queue - sets the format of the data to a queue (FIFO).
+ * @h: The given stack.
+ * @ipn: The input line number.
+ *
+ * Return: Void.
+ */
+
+void m_queue(stack_t **h, unsigned int ipn)
+{
+	(void)h;
+	(void)ipn;
+	driver.is_queue = true;
+}
