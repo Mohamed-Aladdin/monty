3078109fdaf7f28edf29dbfd86e9489f02eb536f
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-01-16 19:24:35 +0000

    Fix no of fns per file limitations

--- op_helpers2.c
+++ op_helpers2.c
@@ -156,34 +156,3 @@ void m_rotl(stack_t **h, __attribute__((unused))unsigned int ipn)
 	ft->next = NULL;
 	ft->prev = sd;
 }
-
-/**
- * m_rotr - rotates the stack to the bottom.
- * @h: The given stack.
- * @ipn: The input line number.
- *
- * Return: Void.
- */
-
-void m_rotr(stack_t **h, __attribute__((unused))unsigned int ipn)
-{
-	stack_t *ft, *sd;
-
-	if (!(*h) || !(*h)->next)
-	{
-		return;
-	}
-
-	ft = *h;
-
-	while (ft->next->next)
-	{
-		ft = ft->next;
-	}
-	sd = ft->next;
-	ft->next = NULL;
-	(*h)->prev = sd;
-	sd->next = *h;
-	sd->prev = NULL;
-	*h = sd;
-}
